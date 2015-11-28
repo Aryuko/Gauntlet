3e9fcf67f3eb783f6f04a91d720ae05025669d51
Zalodu <peter.kjellen@gmail.com>
2015-11-28 12:34:57 +0100

    initial flex test

---
+++ view/progress.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Progress</title>
+		<link href="generalStyle.css" rel="stylesheet"/>
+	</head>
+	<body>
+		<div class="progressbar">
+			<div class="barSection">
+				
+			</div>
+			<div class="barSection">
+				
+			</div>
+			<div class="barSection">
+				
+			</div>
+			<div class="barSection">
+				
+			</div>
+			<div class="barSection">
+				
+			</div>
+		</div>
+	</body>
+</html>
+
+<style>
+	.progressbar{
+		background-color: #909090;
+		height: 200px;
+		width: 500px;
+		display: flex;
+	}
+	.barSection{
+		height: 200px;
+	}
+</style>
