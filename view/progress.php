760f888c104b56d54912a0a6e8e3177db123235c
Zalodu <peter.kjellen@gmail.com>
2015-11-28 12:59:42 +0100

    .

--- view/progress.php
+++ view/progress.php
@@ -26,14 +26,19 @@
 
 <style>
 	.progressbar{
+		margin: auto;
+		position: relative;
+		top: 25%;
 		background-color: #909090;
 		width: 500px;
 		display: flex;
+		border-radius: 20px;
+		overflow: hidden;
 	}
 	.barSection{
-		height: 200px;
-		background-color: #2a8e22;
-		min-width: 20%;
+		height: 100px;
+		background-color: #38b12f;
+		min-width: 1%;
 		flex: 1;
 		border-left: 2px solid #777777;
 		border-right: 2px solid #4a4a4a;
