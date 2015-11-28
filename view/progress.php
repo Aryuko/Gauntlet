f61469d70994d46dc451fcf49fdc32aa9a53b712
Zalodu <peter.kjellen@gmail.com>
2015-11-28 13:23:41 +0100

    .

--- view/progress.php
+++ view/progress.php
@@ -34,13 +34,14 @@
 		display: flex;
 		border-radius: 20px;
 		overflow: hidden;
+		border: 3px solid #c6c6c6;
 	}
 	.barSection{
 		height: 100px;
 		background-color: #38b12f;
 		min-width: 1%;
 		flex: 1;
-		border-left: 2px solid #777777;
-		border-right: 2px solid #4a4a4a;
+		border-left: 2px solid #c6c6c6;
+		border-right: 2px solid #9d9d9d;
 	}
 </style>
