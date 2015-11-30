105a9b663f49c243a87a55641781122d7b46f02a
pekj <pekj@kth.se>
2015-11-30 14:11:42 +0100

    use query instead of execute

--- controller/database/getProgress.php
+++ controller/database/getProgress.php
@@ -4,6 +4,7 @@
 
 	$stmt = $dbh->prepare("SELECT `progress` FROM `gProgress` WHERE `id` = 0");
 
+    /*
 	if(!$stmt->execute()){
        die('Could not execute statement');
     }
@@ -11,9 +12,14 @@
 	if(!$res = $stmt->get_result()){
        die('Could not get result');
     }
+    */
+
+    if(!$res = $dbh->query($stmt)){
+        die('Could not get result')
+    }
 
 	if(!$row = $res->fetch_row()){
-       die('Could not fetch array');
+       die('Could not fetch row');
     }
 
 	echo $row[0];
