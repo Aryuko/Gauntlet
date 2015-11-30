67a7b36914fd98db5f425066761fa9cd1281d558
pekj <pekj@kth.se>
2015-11-30 13:22:27 +0100

    remove mysql_errors

--- controller/database/getProgress.php
+++ controller/database/getProgress.php
@@ -5,15 +5,15 @@
 	$stmt = $dbh->prepare("SELECT `progress` FROM `gProgress` WHERE `id` = 0");
 
 	if(!$stmt->execute()){
-       die('Could not execute statement' . mysql_error());
+       die('Could not execute statement');
     }
 
 	if($res = $stmt->get_result()){
-       die('Could not get result' . mysql_error());
+       die('Could not get result');
     }
 
 	if($row = $res->fetch_array()){
-       die('Could not fetch array' . mysql_error());
+       die('Could not fetch array');
     }
 
 	echo $row[0];
