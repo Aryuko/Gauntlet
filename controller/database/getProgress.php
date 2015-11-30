900ce78c1ca9af179144bbca3331d9a92aa112fd
pekj <pekj@kth.se>
2015-11-30 13:02:02 +0100

    fix syntax error

--- controller/database/getProgress.php
+++ controller/database/getProgress.php
@@ -5,15 +5,15 @@
 	$stmt = $dbh->prepare("SELECT `progress` FROM `gProgress` WHERE `id` = 0");
 
 	if(!$stmt->execute()){
-       die('Could not execute statement' . mysql_error())
+       die('Could not execute statement' . mysql_error());
     }
 
 	if($res = $stmt->get_result()){
-       die('Could not get result' . mysql_error())
+       die('Could not get result' . mysql_error());
     }
 
 	if($row = $res->fetch_array()){
-       die('Could not fetch array' . mysql_error())
+       die('Could not fetch array' . mysql_error());
     }
 
 	echo $row[0];
